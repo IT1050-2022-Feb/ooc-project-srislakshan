2f3f057e2d85517b48032d4167a5b47b505ab2ed
srislakshan <105860026+srislakshan@users.noreply.github.com>
2022-05-19 17:25:38 +0530

    Add files via upload

---
+++ ooc/app.cpp
@@ -0,0 +1,10 @@
+app::app(int Id,string Name,string desc,string fpath) {
+  //implementation goes here
+}
+void app::update_app() {
+  //implementation goes here
+}
+
+void app::get_app_details() {
+  //implementation goes here
+}
